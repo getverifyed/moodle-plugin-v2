565ffa70cabd647e450cf2a3b2e058eebd1ddae6
Michael J. Fordham <michael97f@gmail.com>
2023-05-30 11:40:47 +0100

    updated lib with more functionality

--- verifyed/lib.php
+++ verifyed/lib.php
@@ -1,26 +1,155 @@
 <?php
 
-// Function for adding VerifyEd instance to course
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Add a new instance of VerifyEd to course
+ *
+ * @param stdClass $verifyed
+ * @return int
+ */
 function verifyed_add_instance($verifyed) {
-    // Code for adding an instance of VerifyEd to a Moodle course goes here
+    global $DB;
+
+    // Save plugin instance to database
+    $verifyed->id = $DB->insert_record('verifyed', $verifyed);
+
+    // Get course data from Moodle
+    $moodle_course = $DB->get_record('course', array('id' => $verifyed->course), 'fullname, shortname, summary');
+
+    // Set course data based on requirements
+    $course_data = array(
+        "name" => $moodle_course->fullname,
+        "information" => $moodle_course->summary,
+        "courseskills" => [],
+        "validity" => array(
+            "validityNumber" => 0,
+            "validityLength" => "year(s)"
+        ),
+        "durationValues" => array(
+            "durationNumber" => 0,
+            "durationLength" => "year(s)"
+        ),
+        "courseLink" => "",
+        "courseCode" => $moodle_course->shortname,
+        "referencedCourses" => [],
+        "additionalInfo" => []
+    );
+
+    // Create a course in VerifyEd
+    $verifyed_course_id = verifyed_create_course($course_data);
+
+    // Save VerifyEd course ID to database
+    $verifyed_course_map = new stdClass();
+    $verifyed_course_map->course_id = $verifyed->course;
+    $verifyed_course_map->verifyed_course_id = $verifyed_course_id;
+    $DB->insert_record('verifyed_course_map', $verifyed_course_map);
+
+    return $verifyed->id;
 }
 
-// Function for updating VerifyEd instance
+/**
+ * Update an existing VerifyEd instance
+ *
+ * @param stdClass $verifyed
+ * @return bool
+ */
 function verifyed_update_instance($verifyed) {
-    // Code for updating an instance of VerifyEd goes here
+    global $DB;
+
+    // Update plugin instance in the database
+    $DB->update_record('verifyed', $verifyed);
+
+    return true;
 }
 
-// Function for creating a course in VerifyEd
+/**
+ * Create a course in VerifyEd
+ *
+ * @param array $course_data
+ * @return int
+ */
 function verifyed_create_course($course_data) {
-    // Code for making an API call to VerifyEd to create a course
+    // Prepare JSON data for the API request
+    $data = json_encode($course_data);
+
+    // Initialize cURL session
+    $ch = curl_init('https://api.verifyed.io/external/courses?apiKey=' . urlencode(get_config('verifyed', 'apikey')) . '&type=institution');
+
+    // Set cURL options
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($data))
+    );
+
+    // Execute cURL request and close the session
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    // Decode the JSON response
+    $result = json_decode($response, true);
+
+    // Return the VerifyEd course ID
+    return $result['data']['id'];
 }
 
-// Function for issuing a certificate in VerifyEd
+/**
+ * Issue a certificate in VerifyEd
+ *
+ * @param array $certificate_data
+ * @return void
+ */
 function verifyed_issue_certificate($certificate_data) {
-    // Code for making an API call to VerifyEd to issue a certificate
+    // Prepare JSON data for the API request
+    $data = json_encode($certificate_data);
+
+    // Initialize cURL session
+    $ch = curl_init('https://api.verifyed.io/external/issue-credentials?apiKey=' . urlencode(get_config('verifyed', 'apikey')) . '&type=partner');
+
+    // Set cURL options
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($data))
+    );
+
+    // Execute cURL request and close the session
+    $response = curl_exec($ch);
+    $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    // Decode the JSON response
+    $result = json_decode($response, true);
+
+    // Check if the certificate has been issued successfully
+    if ($http_status === 200 && isset($result['learnerData']) && !empty($result['learnerData'])) {
+        // Display success message
+        echo html_writer::tag('p', 'Your certificate has been issued. Check your emails for a link to see it.');
+    } else {
+        // Display error message
+        echo html_writer::tag('p', 'Sorry, your certificate could not be issued right now. You should contact your institution to resolve the problem and claim your certificate.');
+    }
 }
 
-// Function for checking whether a student already has a certificate issued
+/**
+ * Check whether a student already has a certificate issued
+ *
+ * @param int $user_id
+ * @param int $course_id
+ * @return bool
+ */
 function verifyed_has_certificate($user_id, $course_id) {
-    // Code for checking if a certificate has already been issued to the student
+    global $DB;
+
+    // Query the database to check if a certificate has already been issued to the student
+    $sql = "SELECT COUNT(*) FROM {verifyed_certificates} WHERE userid = ? AND courseid = ?";
+    $params = array($user_id, $course_id);
+    $count = $DB->count_records_sql($sql, $params);
+
+    return $count > 0;
 }
